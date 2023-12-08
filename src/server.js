45f6e05f32e6507091919e877132d467ff1e2abe
Carlos Valencia Henao <rasfreed@gmail.com>
2023-12-08 14:50:01 -0500

    a new origin to request was added

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ connect()
 
 app.use(express.json())
 app.use(cors({
-  origin: [process.env.FRONTEND, process.env.DEV, 'http://localhost:3000']
+  origin: [process.env.FRONTEND, process.env.ADMON, process.env.DEV, 'http://localhost:3000']
 }))
 app.use(morgan('dev'))
 
